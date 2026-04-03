1c7093879aa814086c373176fa82fe422bce36f1
sumedhachowdary-coder <sumedha.challagundla2023@vitstudent.ac.in>
2026-04-03 20:18:31 +0530

    v2 - Search Donor feature

---
+++ v2/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let donors = [];
+
+app.post('/add', (req, res) => {
+    donors.push(req.body);
+    res.send("Donor added successfully");
+});
+
+app.get('/', (req, res) => {
+    res.send("Blood Bank v1 running");
+});
+app.get('/search/:bloodGroup', (req, res) => {
+    const result = donors.filter(d => d.bloodGroup === req.params.bloodGroup);
+    res.json(result);
+});
+app.listen(3000, () => console.log("Server running on port 3000"));
